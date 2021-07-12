affb6bbeef5de810a7bb81b3be501dc8f6aeb085
Facesoccer <80411445+RenatoMoratto@users.noreply.github.com>
2021-07-12 11:58:22 -0300

    Work in progress
    Create PlayerName component with some CSS

--- matching-game/main.js
+++ matching-game/main.js
@@ -7,9 +7,12 @@ import BoardGame from './src/object/BoardGame';
 import PlayerName from './src/components/PlayerName';
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
-// const $hmtlPlayerName = PlayerName(1) + PlayerName(2);
 
-// $root.insertAdjacentHTML('beforebegin', $hmtlPlayerName);
-$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
-
+$root.insertAdjacentHTML(
+    'beforebegin', 
+    `
+        ${PlayerName('Player1')}
+        ${PlayerName('Player2')}
+        ${BoardGame(6)}
+    `
+);
